7b78bb6dfbe2be3676103abc123127ad540e7b37
Dharundds <76607616+Dharundds@users.noreply.github.com>
2021-10-07 21:46:24 +0530

    test

--- server/app.py
+++ server/app.py
@@ -1,18 +1,28 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route("/getdata", methods=["GET"])
+
+def readDisplay():
+    data = pd.read_csv(
+        "datasets/areaProduction.csv")
+    df = pd.DataFrame(data)
+    for index, row in df.iterrows():
+        if 'Hot' in row["Climate"]:
+            print(row.str.get('CropName'))
+            break
+    # print(df['Soil'])
+
+
+@app.route("/", methods=["GET"])
 def index():
-    # return jsonify({
-    #     "Centigrade": "Agroxpert",
-    #     "Developed by": ["Dharundds", "DharunVS","Ramya", "HrithikMJ"]
-    # })
-
-    return jsonify({"crops": ["rice", "wheat"]})
-
+    return jsonify({
+        "Centigrade": "Agroxpert",
+        "Developed by": ["Dharundds", "DharunVS", "Ramya", "HrithikMJ"]
+    })
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=6900)
-
+    # app.run(debug=True, port=6900)
+    readDisplay()
