4c20a630016490373bfb94c11f9cab26af1a0b84
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-10-08 22:43:34 +0530

    Updates

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 
 function App() {
-
-  
-
-  return <div className="App">AgroExpert</div>;
+  return (
+    <div className="App">
+      <h1>AgroXpert</h1>
+      <form></form>
+    </div>
+  );
 }
 
 export default App;
--- server/app.py
+++ server/app.py
@@ -4,15 +4,29 @@ import requests
 
 app = Flask(__name__)
 
-data = pd.read_csv("server/datasets/TN_data.csv")
+data = pd.read_csv("datasets/TN_data.csv")
 df = pd.DataFrame(data)
 
 
-def get_distinct():   #param : col
+def get_distinct(col):   #param : col
     #li = list(set(df[col].to_list()))
-    group = df.groupby('District_Name')
+    group = df.groupby(col)
     df2 = group.apply(lambda x: x['Crop'].unique())
     return df2
+
+
+def get_yield(district):
+    
+    df2 = get_distinct("District_Name")
+    crops = df2[district]
+
+    for crop in crops:
+        area = df.loc[(df['District_Name'] == district) & (df['Crop'] == crop) & (df["Area"].notnull() )& (df["Production"].notnull() )] ["Area"].to_list()
+        production = df.loc[(df['District_Name'] == district) & (df['Crop'] == crop)  & (df["Area"].notnull()) & (df["Production"].notnull())]["Production"].to_list()
+        crop_yield = sum(production)/sum(area)
+        
+        print(crop, crop_yield)
+
 """
 @app.route('/getcrop/<district>/<name>')
 def getCrop(district, name):
@@ -36,6 +50,7 @@ def index():
 if __name__ == "__main__":
     app.run(debug=True, port=6900)
 """
-print(get_distinct())
+
+get_yield("ARIYALUR")
 
 
