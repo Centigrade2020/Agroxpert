c4ac464421a9b7e6dab86f8b1d5c2f4cd083ff46
dharunvs <dharunsivakumar002@gmail.com>
2021-10-15 19:59:19 +0530

    Updates

--- v2/client/src/components/index.js
+++ v2/client/src/components/index.js
@@ -2,5 +2,6 @@ import Chart from "./Chart";
 import Navbar from "./Navbar";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
+import CropSuggestion from "./CropSuggestion";
 
-export { Chart, Navbar, LoginButton, LogoutButton };
+export { Chart, Navbar, LoginButton, LogoutButton, CropSuggestion };
--- v2/client/src/pages/Dashboard/Dashboard.jsx
+++ v2/client/src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import { CropSuggestion } from "../../components";
+
 function Dashboard() {
-  return <div>DASHBOARD</div>;
+  const [tab, setTab] = useState("Your Trackings");
+
+  const staticTabs = [
+    "Your Trackings",
+    "Crop Suggestions",
+    "Predict Production",
+  ];
+  return (
+    <div className="Dashboard">
+      <section className="tabs">
+        <ul>
+          {staticTabs.map((value, key) => (
+            <li
+              key={key}
+              onClick={() => {
+                setTab(value);
+              }}
+              value={value}
+              className={tab === value ? "active" : ""}
+            >
+              {value}
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className="right">
+        {tab === "Crop Suggestions" ? (
+          <CropSuggestion />
+        ) : (
+          <h1>Your Trackings</h1>
+        )}
+      </section>
+    </div>
+  );
 }
 
 export default Dashboard;
